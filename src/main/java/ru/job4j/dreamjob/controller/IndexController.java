3a6df3fe208e6c133980d332bf3c1661b329a0f4
devvk <info@sekretov.net>
2026-05-02 14:27:28 +0200

    init

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@SuppressWarnings("unused")
 public class IndexController {
+
     @GetMapping("/index")
     public String getIndex() {
         return "index";
