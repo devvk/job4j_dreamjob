2f7e42c40fd0773df6cc0396a9f7351b6138269b
devvk <info@sekretov.net>
2026-05-06 12:29:05 +0200

    4. Thymeleaf, Циклы. [#504841 #563470]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -42,13 +42,10 @@ public class MemoryVacancyRepository implements VacancyRepository {
 
     @Override
     public boolean update(Vacancy vacancy) {
-        return vacancies.computeIfPresent(vacancy.getId(),
-                (id, oldVacancy) -> new Vacancy(
-                        oldVacancy.getId(),
-                        vacancy.getTitle(),
-                        vacancy.getDescription()
-                )
-        ) != null;
+        return vacancies.computeIfPresent(vacancy.getId(), (id, oldVacancy) -> {
+            vacancy.setCreationDate(oldVacancy.getCreationDate());
+            return vacancy;
+        }) != null;
     }
 
     @Override
