cf452521c6959a5af884802cc2b212b0d74afa66
devvk <info@sekretov.net>
2026-05-08 20:05:02 +0200

    4. Многопользовательские приложения. Многопоточность в Веб приложении. [#504857]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -1,21 +1,22 @@
 package ru.job4j.dreamjob.repository;
 
+import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Repository;
 import ru.job4j.dreamjob.model.Candidate;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
+@ThreadSafe
 public class MemoryCandidateRepository implements CandidateRepository {
 
-    private int nextId = 1;
+    private final AtomicInteger nextId = new AtomicInteger(0);
 
-    private final Map<Integer, Candidate> candidates = new HashMap<>();
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
-    private MemoryCandidateRepository() {
+    public MemoryCandidateRepository() {
         save(new Candidate(0, "Alex Petrov", "Intern Java Developer"));
         save(new Candidate(0, "Werner Siemens", "Junior Java Developer"));
         save(new Candidate(0, "Lucas Schmidt", "Junior+ Java Developer"));
@@ -26,7 +27,7 @@ public class MemoryCandidateRepository implements CandidateRepository {
 
     @Override
     public Candidate save(Candidate candidate) {
-        candidate.setId(nextId++);
+        candidate.setId(nextId.getAndIncrement());
         candidates.put(candidate.getId(), candidate);
         return candidate;
     }
@@ -51,6 +52,6 @@ public class MemoryCandidateRepository implements CandidateRepository {
 
     @Override
     public Collection<Candidate> findAll() {
-        return candidates.values();
+        return List.copyOf(candidates.values());
     }
 }
--- src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -1,21 +1,22 @@
 package ru.job4j.dreamjob.repository;
 
+import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Repository;
 import ru.job4j.dreamjob.model.Vacancy;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
+@ThreadSafe
 public class MemoryVacancyRepository implements VacancyRepository {
 
-    private int nextId = 1;
+    private final AtomicInteger nextId = new AtomicInteger(0);
 
-    private final Map<Integer, Vacancy> vacancies = new HashMap<>();
+    private final Map<Integer, Vacancy> vacancies = new ConcurrentHashMap<>();
 
-    private MemoryVacancyRepository() {
+    public MemoryVacancyRepository() {
         save(new Vacancy(0, "Intern Java Developer", "Backend"));
         save(new Vacancy(0, "Junior Java Developer", "Backend"));
         save(new Vacancy(0, "Junior+ Java Developer", "Backend"));
@@ -26,7 +27,7 @@ public class MemoryVacancyRepository implements VacancyRepository {
 
     @Override
     public Vacancy save(Vacancy vacancy) {
-        vacancy.setId(nextId++);
+        vacancy.setId(nextId.getAndIncrement());
         vacancies.put(vacancy.getId(), vacancy);
         return vacancy;
     }
@@ -51,6 +52,6 @@ public class MemoryVacancyRepository implements VacancyRepository {
 
     @Override
     public Collection<Vacancy> findAll() {
-        return vacancies.values();
+        return List.copyOf(vacancies.values());
     }
 }
